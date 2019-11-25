47c7acf6f4153746dff6744978ad881702fa3d22
vanessadants <vanessa.dantas796@gmail.com>
2019-11-25 19:04:44 -0300

    experimenting with optional and default @RequestParam

--- src/main/java/com/example/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/example/mobileappws/ui/controller/UserController.java
@@ -2,9 +2,11 @@ package com.example.mobileappws.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping
-    public String getUser(){
-        return "get user was called";
+    public String getUser(@RequestParam(value = "page", defaultValue = "1") int page,
+                          @RequestParam(value = "limit", defaultValue = "50") int limit){
+        return "get user was called, with page = " + page + ", limit = " + limit;
+    }
+
+    @GetMapping(path="/{userId}")
+    public String getUser(@PathVariable String userId){
+        return "get user was called with userId = "+userId;
     }
 
     @PostMapping
