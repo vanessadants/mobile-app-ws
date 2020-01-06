3d6be94db403544d80b92aaa645c8b6466385833
vanessa.costa <vanessa.costa@wirecard.com>
2020-01-06 13:23:44 -0300

    adding lombok in project

--- src/main/java/com/example/mobileappws/ui/model/request/UserRequest.java
+++ src/main/java/com/example/mobileappws/ui/model/request/UserRequest.java
@@ -1,69 +1,13 @@
 package com.example.mobileappws.ui.model.request;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class UserRequest {
     private String firstName;
     private String lastName;
     private String email;
     private String password;
-
-    private UserRequest(UserRequestBuilder builder) {
-        this.firstName = builder.firstName;
-        this.lastName = builder.lastName;
-        this.email = builder.email;
-        this.password = builder.password;
-    }
-
-    public static UserRequestBuilder builder(){
-        return new UserRequestBuilder();
-    }
-
-    public static class UserRequestBuilder {
-        private String firstName;
-        private String lastName;
-        private String email;
-        private String password;
-
-        private UserRequestBuilder() {
-        }
-
-        public UserRequestBuilder withFirstName(String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-
-        public UserRequestBuilder withLastName(String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-
-        public UserRequestBuilder withEmail(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public UserRequestBuilder withPassword(String password) {
-            this.password = password;
-            return this;
-        }
-
-        public UserRequest build(){
-            return new UserRequest(this);
-        }
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
--- src/main/java/com/example/mobileappws/ui/model/response/UserResponse.java
+++ src/main/java/com/example/mobileappws/ui/model/response/UserResponse.java
@@ -1,69 +1,13 @@
 package com.example.mobileappws.ui.model.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class UserResponse {
     private String userId;
     private String firstName;
     private String lastName;
     private String email;
-
-    private UserResponse(UserResponseBuilder builder) {
-        this.userId = builder.userId;
-        this.firstName = builder.firstName;
-        this.lastName = builder.lastName;
-        this.email = builder.email;
-    }
-
-    public static UserResponseBuilder builder(){
-        return new UserResponseBuilder();
-    }
-
-    public static class UserResponseBuilder {
-        private String userId;
-        private String firstName;
-        private String lastName;
-        private String email;
-
-        private UserResponseBuilder() {
-        }
-
-        public UserResponseBuilder withUserId(String userId) {
-            this.userId = userId;
-            return this;
-        }
-
-        public UserResponseBuilder withFirstName(String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-
-        public UserResponseBuilder withLastName(String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-
-        public UserResponseBuilder withEmail(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public UserResponse build(){
-            return new UserResponse(this);
-        }
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 }
