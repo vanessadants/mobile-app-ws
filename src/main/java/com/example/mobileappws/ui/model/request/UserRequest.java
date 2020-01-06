b8346193f884e2622ea94f8a8e2851e323e0af0d
vanessa.costa <vanessa.costa@wirecard.com>
2020-01-06 13:51:07 -0300

    adding validations on the UserRequest

--- src/main/java/com/example/mobileappws/ui/model/request/UserRequest.java
+++ src/main/java/com/example/mobileappws/ui/model/request/UserRequest.java
@@ -3,11 +3,24 @@ package com.example.mobileappws.ui.model.request;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Builder
 @Getter
 public class UserRequest {
+    @NotNull(message = "First name cannot be null")
     private String firstName;
+
+    @NotNull(message = "Last name cannot be null")
     private String lastName;
+
+    @NotNull(message = "Email cannot be null")
+    @Email
     private String email;
+
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, max = 16, message = "Password must be equal or greater than 8 characters and less than 16 characters")
     private String password;
 }
